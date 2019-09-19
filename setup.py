f5f4549a57680791570c4f8e6211a1b3d0913ff0
Paras Jain <parasj96@gmail.com>
2019-09-19 16:20:51 -0700

    Add remat version tag

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 
 setup(name="keras_segmentation",
-      version="0.2.0",
+      version="0.2.0.remat",
       description="Image Segmentation toolkit for keras",
       author="Divam Gupta",
       author_email='divamgupta@gmail.com',
